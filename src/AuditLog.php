b515a17121922eb9abc76df5aa2c9d9afa6066e6
KeironMcGivern <KeironMcGivern@users.noreply.github.com>
2020-11-07 10:51:30 +0000

    Fix styling

--- src/AuditLog.php
+++ src/AuditLog.php
@@ -20,7 +20,7 @@ class AuditLog
 
     public function causer(?Model $causer = null): AuditLog
     {
-        if (!$causer) {
+        if (! $causer) {
             $this->activity->causedByAnonymous();
         } else {
             $this->activity->causedBy($causer);
